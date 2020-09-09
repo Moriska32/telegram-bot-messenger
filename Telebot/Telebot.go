dcc6fd1400e48ac8fb607570910ad63b8e4ca9b9
Верниковский Иван Юрьевич <Vernikovsky_IY@mgtniip.ru>
2020-09-09 16:02:46 +0300

    first commit

---
+++ Telebot/Telebot.go
@@ -0,0 +1,52 @@
+package telebot
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
+)
+
+type configJSON struct {
+	BotKey string `json:"bot-key"`
+}
+
+//TelebotInit initialization bot
+func telebotInit() {
+
+	jsonFile, err := os.Open("config.json")
+	defer jsonFile.Close()
+	// if we os.Open returns an error then handle it
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	var config configJSON
+
+	byteValue, _ := ioutil.ReadAll(jsonFile)
+
+	json.Unmarshal(byteValue, &config)
+
+	bot, err := tgbotapi.NewBotAPI(config.BotKey)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	bot.Debug = true
+
+	log.Printf("Authorized on account %s", bot.Self.UserName)
+
+	u := tgbotapi.NewUpdate(0)
+	u.Timeout = 60
+
+	updates, err := bot.GetUpdatesChan(u)
+
+	for update := range updates {
+		if update.Message == nil { // ignore any non-Message Updates
+			continue
+		}
+	}
+}
